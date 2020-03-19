9a07b960c28787d4def787bb3af7d9e2315f2c44
Gabrielle Wildfeuer <gpwildfeuer@email.wm.edu>
2020-03-19 19:29:08 +0100

    idk

--- database/migrations/2020_03_19_180824_create_organizations_table.php
+++ database/migrations/2020_03_19_180824_create_organizations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateOrganizationsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('organizations', function (Blueprint $table) {
             $table->unsignedBigInteger('id');
-            $table->string('first_name');
-            $table->string('surname');
+            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
@@ -32,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('organizations');
     }
 }
