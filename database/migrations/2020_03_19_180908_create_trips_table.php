25e53bc04d4c8bd3609dbc9390cb39369020c5d1
soaresilva <ciente@gmail.com>
2020-03-19 19:24:37 +0100

    migration

---
+++ database/migrations/2020_03_19_180908_create_trips_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTripsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->unsignedBigInteger('event_id')->nullable();
+            $table->integer('distance');
+            $table->float('carbon_amount', 8, 3);
+            $table->dateTime('flight_date')->nullable();
+            $table->string('airport_from')->nullable();
+            $table->string('airport_to')->nullable();
+            $table->timestamps()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trips');
+    }
+}
