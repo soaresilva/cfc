291dce8e617e9207e6ee2d10b83788799b06164d
soaresilva <ciente@gmail.com>
2020-03-19 19:36:58 +0100

    fixed migration tables

--- database/migrations/2020_03_19_181143_create_events_table.php
+++ database/migrations/2020_03_19_181143_create_events_table.php
@@ -13,7 +13,7 @@ class CreateEventsTable extends Migration
      */
     public function up()
     {
-        Schema::table('events', function (Blueprint $table) {
+        Schema::create('events', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->unsignedBigInteger('organization_id');
