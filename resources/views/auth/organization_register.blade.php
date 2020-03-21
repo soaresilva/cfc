c1f92e15a0f3d4c62450113782d2dc358424681a
Gabrielle Wildfeuer <gpwildfeuer@email.wm.edu>
2020-03-21 11:48:51 +0100

    fixed OrganizationRegister blade

--- resources/views/auth/organization_register.blade.php
+++ resources/views/auth/organization_register.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Organization_Register') }}</div>
+                <div class="card-header">{{ __('Organization Register') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('OrganizationRegistrationController@register') }}">
