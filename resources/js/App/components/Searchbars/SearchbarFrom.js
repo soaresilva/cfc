f49e77654dda778c80a8f9d6a94010dbc3e2f201
soaresilva <ciente@gmail.com>
2020-03-25 16:34:01 +0100

    list of airports

--- resources/js/App/components/Searchbars/SearchbarFrom.js
+++ resources/js/App/components/Searchbars/SearchbarFrom.js
@@ -1,25 +1,41 @@
 import React from "react";
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from "reactstrap";
+import {
+    Dropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+} from "reactstrap";
+// import TextField from "@material-ui/core/TextField";
+// import Autocomplete from "@material-ui/lab/Autocomplete";
 
 import "./Searchbar.css";
 
 function searchBar(props) {
-  const { dropdownOpen, dropDownClickHandler, flightsFrom, itemSelectFromHandler } = props;
-  let flightFrom = flightsFrom.map(item => {
-    return <DropdownItem key={item} onClick={itemSelectFromHandler}>{item}</DropdownItem>;
-  });
+    const {
+        dropdownOpen,
+        dropDownClickHandler,
+        flightsFrom,
+        itemSelectFromHandler
+    } = props;
+    let flightFrom = flightsFrom.map(item => {
+        return (
+            <DropdownItem key={item} onClick={itemSelectFromHandler}>
+                {item}
+            </DropdownItem>
+        );
+    });
 
-  return (
-    <div className="DropItem">
-      <Dropdown isOpen={dropdownOpen} toggle={dropDownClickHandler}>
-        <DropdownToggle caret>{props.children}</DropdownToggle>
-        <DropdownMenu>
-          <DropdownItem header>{props.children}</DropdownItem>
-          {flightFrom}
-        </DropdownMenu>
-      </Dropdown>
-    </div>
-  );
+    return (
+        <div className="DropItem">
+            <Dropdown isOpen={dropdownOpen} toggle={dropDownClickHandler}>
+                <DropdownToggle caret>{props.children}</DropdownToggle>
+                <DropdownMenu>
+                    <DropdownItem header>{props.children}</DropdownItem>
+                    {flightFrom}
+                </DropdownMenu>
+            </Dropdown>
+        </div>
+    );
 }
 
 export default searchBar;
