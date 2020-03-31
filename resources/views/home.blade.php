1d162af0ae7cee4728a9c97a3a8e7f3d256c0bdd
soaresilva <ciente@gmail.com>
2020-03-31 15:58:45 +0200

    link to main page on user profile, styling

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,12 @@
     <div class="col-md-10 col-sm-8 col-xs-6">
       <div class="card">
         <div class="card-header">{{ Auth::user()->first_name }} {{ Auth::user()->surname  }}'s Profile</div>
-        <div class="card-body">
+        <div class="card-body card-body-link">
+          <a href="{{ url('/') }}">
+            Search for flights!
+          </a>
+        </div>
+        <div class="card-body card-body-chart">
           @if (session('status'))
           <div class="alert alert-success" role="alert">
             {{ session('status') }}
@@ -14,7 +19,7 @@
           @endif
           {!! $userChart->container() !!}
         </div>
-        <div id="userProfile" class="card-body"></div>
+        <div id="userProfile" class="card-body card-body-react"></div>
         {!! $userChart->script() !!}
       </div>
     </div>
