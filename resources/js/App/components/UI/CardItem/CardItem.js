90a838055dae095f91b722c972233bcecbca6823
soaresilva <ciente@gmail.com>
2020-04-02 18:51:56 +0200

    removing styles

--- resources/js/App/components/UI/CardItem/CardItem.js
+++ resources/js/App/components/UI/CardItem/CardItem.js
@@ -84,13 +84,13 @@ function CardItem(props) {
       children === "Register" ? (
         <div className="CardItemsLandingPage">
           <h1>{children}</h1>
-          <div>
-            <p className="CardItemsLandingPage_description">Register an account to access extra features:</p>
-            <ul className="CardItemsLandingPage_features">
-              <li>Add trips to your profile</li>
-              <li>See your trip history and your flights' carbon footprint</li>
-              <li>Organizations can create events and calculate their footprint</li>
-            </ul>
+          <div className="CardItemsLandingPage_description">
+            <p>Register an account to access extra features:</p>
+            <div className="CardItemsLandingPage_features">
+              <p>Add trips to your profile</p>
+              <p>See your trip history and your flights' carbon footprint</p>
+              <p>Organizations can create events and calculate their footprint</p>
+            </div>
           </div>
           <div className="CardItemsLandingPage__links">
             <a href="/register" className="CardItemsLandingPage__Alinks">
@@ -106,8 +106,8 @@ function CardItem(props) {
           <h1>{children}</h1>
           <div className="CardItemsLandingPage_description">
             <p>
-              Carbon Voyage calculates the carbon footprint of your flights, helping you and your organization make more informed
-              decisions when it comes to flying across the globe.
+              Carbon Voyage calculates the carbon footprint of your flights, helping you and your organization make more informed decisions
+              when it comes to flying across the globe.
             </p>
             <p>Maybe your seminar could be a webinar instead!</p> <p>Make a concrete commitment for a more sustainable future.</p>
           </div>
@@ -115,12 +115,12 @@ function CardItem(props) {
       ) : (
         <div className="CardItemsLandingPage">
           <h1>{children}</h1>
-          <div>
-            <p className="CardItemsLandingPage_description">Offset the carbon footprint of your flights through our trusted partners</p>
-            <ul className="CardItemsLandingPage_features">
-              <li>Invest in sustainable development projects in newly industrialising countries</li>
-              <li>Finance citizen-led clean energy projects</li>
-            </ul>
+          <div className="CardItemsLandingPage_description">
+            <p>Offset the carbon footprint of your flights through our trusted partners</p>
+            <div className="CardItemsLandingPage_features">
+              <p>Invest in sustainable development projects in newly industrialising countries</p>
+              <p>Finance citizen-led clean energy projects</p>
+            </div>
           </div>
           <div className="CardItemsLandingPage__icons" onClick={scrollToFlightsSection}>
             <p>Let's start, shall we?</p>
